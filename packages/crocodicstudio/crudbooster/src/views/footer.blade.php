f732cd6e6db45e93c586e00f0c48668aca2cf42a
Riccardo Amadi <amadi.riccardo@gmail.com>
2020-04-27 12:09:28 +0200

    add version macro.micro in footer

--- packages/crocodicstudio/crudbooster/src/views/footer.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/footer.blade.php
@@ -1,7 +1,10 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-{{ trans('crudbooster.right') }} hidden-xs">
-        {{ trans('crudbooster.powered_by') }} {{Session::get('appname')}}
+        {{ trans('crudbooster.powered_by') }} Data Synapsi
+    </div>
+    <div style="margin-right:15px;" class="pull-{{ trans('crudbooster.right') }} hidden-xs">
+        {{Session::get('appname')}} v{{config('app.version')}}
     </div>
     <!-- Default to the left -->
     <strong>{{ trans('crudbooster.copyright') }} &copy; <?php echo date('Y') ?>. {{ trans('crudbooster.all_rights_reserved') }} .</strong>
