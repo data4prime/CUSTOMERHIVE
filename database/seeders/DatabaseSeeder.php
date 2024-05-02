542bfb2f3b898a44b7ae3c21645398417dcb0b9d
Marius Donici <marius.donici@data4prime.com>
2024-05-02 14:28:19 +0000

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(CBSeeder::class);
+        //$this->call(CBSeeder::class);
+        $this->command->info('Qlik settings...');
+        $this->call(Qlik_Sett::class);
     }
 }
