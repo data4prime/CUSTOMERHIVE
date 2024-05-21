d91a587daacbd5a2b53bcc677357da06a4a9caea
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-05-21 15:02:08 +0200

    seeders to seeds

---
+++ packages/crocodicstudio/crudbooster/src/database/seeders/Qlik_Conf.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+
+
+class Qlik_Conf extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->command->info('Please wait updating the data...');
+                
+$this->call('QlikConf');
+
+
+
+        $this->command->info('Updating the data completed !');
+    }
+}
+
+
+
+class QlikConf extends Seeder
+{
+    public function run()
+    {
+
+               $mod = [
+                'created_at' => date('Y-m-d H:i:s'),
+                'name' => 'Qlik Configuration',
+                'icon' => 'fa fa-cog',
+                'path' => 'qlik_confs',
+                'table_name' => 'qlik_confs',
+                'controller' => 'QlikConfController',
+                'is_protected' => 1,
+                'is_active' => 1,
+            ];
+        DB::table('cms_moduls')->insert($mod);  
+    }
+}
