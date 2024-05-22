89e0de44bb47a7bb39760a7c58159005d4f82bee
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-05-22 14:27:10 +0200

    modal component debugging

--- database/migrations/2024_05_22_130802_add_columns_qlik_users_table.php
+++ database/migrations/2024_05_22_130802_add_columns_qlik_users_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::table('qlik_users', function (Blueprint $table) {
             //
-            $table->unsignedBigInteger('qlik_conf_id');
-            $table->foreign('qlik_conf_id')->references('id')->on('qlik_confs');
+            //$table->unsignedBigInteger('qlik_conf_id');
+            //$table->foreign('qlik_conf_id')->references('id')->on('qlik_confs');
         });
     }
 
