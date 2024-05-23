ef8f51b85b6a9f49c8934ea00d1f45187fb82825
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-05-23 17:45:20 +0200

    modifica item access

--- packages/crocodicstudio/crudbooster/src/controllers/AdminQlikItemsController.php
+++ packages/crocodicstudio/crudbooster/src/controllers/AdminQlikItemsController.php
@@ -801,7 +801,7 @@ $data = [];
 		$data['help'] = $data['row']->description;
 		$data['subtitle'] = $data['row']->subtitle;
 		$data['item_url'] = $data['row']->url;
-$data['debug'] = $data['item_url'];
+$data['debug'] = $conf->debug;
 
 		$this->cbView('qlik_items.view', $data);
 	}
@@ -871,7 +871,7 @@ $data = [];
 		$data['help'] = $data['row']->description;
 		$data['subtitle'] = $data['row']->subtitle;
 		$data['item_url'] = $data['row']->url;
-		$data['debug'] = $data['item_url'];
+		$data['debug'] = $conf->debug;
 
 		$this->cbView($view, $data);
 	}
