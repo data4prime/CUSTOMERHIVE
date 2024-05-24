d30f1260c0520169ab042b4a036274960808f20b
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-05-24 11:55:31 +0200

    checkox field fixing

--- packages/crocodicstudio/crudbooster/src/views/default/type_components/checkbox/component.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/default/type_components/checkbox/component.blade.php
@@ -1,6 +1,4 @@
-@php 
-dd($form);
-@endphp 
+
 <div class='form-group {{$header_group_class}} {{ ($errors->first($name))?"has-error":"" }}' 
 id='form-group-{{$name}}' style="{!! @$form['style'] !!}">
   <label class='control-label col-sm-2'>{{$form['label']}}
