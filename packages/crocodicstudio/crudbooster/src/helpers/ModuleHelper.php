09e1a5cafecdaf2c1b8b7804ded56c041f8ea0ae
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-05-28 15:11:57 +0200

    Update ModuleHelper.php

--- packages/crocodicstudio/crudbooster/src/helpers/ModuleHelper.php
+++ packages/crocodicstudio/crudbooster/src/helpers/ModuleHelper.php
@@ -198,8 +198,14 @@ if ($module->table == "cms_users") {
     } else {
 
             if ($module->table == "qlik_confs") {
-        $entity_group = DB::table("qlikconfs_groups")->where('group_id', $row->id)->where('group_id',UserHelper::current_user_tenant() )->first()->group_id;
-        $entity_tenant = DB::table("qlikconfs_tenants")->where('tenant_id', $row->id)->where('tenant_id',UserHelper::current_user_tenant() )->first()->tenant_id;
+        $entity_group = DB::table("qlikconfs_groups")->where('group_id', $row->id)->where('group_id',UserHelper::current_user_tenant())->first();//->group_id;
+        $entity_tenant = DB::table("qlikconfs_tenants")->where('tenant_id', $row->id)->where('tenant_id',UserHelper::current_user_tenant() )->first();//->tenant_id;
+              if ($entity_group) {
+                $entity_group = $entity_group->group_id;
+              }
+              if ($entity_tenant) {
+                $entity_tenant = $entity_tenant->tenant_id;
+              }
       }
 
       if ($module->table == "groups") {
