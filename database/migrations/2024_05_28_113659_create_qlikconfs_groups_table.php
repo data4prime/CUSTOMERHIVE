e3306b214679088c72a834ec5000d9edc39421de
Marius Donici <marius.donici@data4prime.com>
2024-05-28 09:52:02 +0000

    qlikconfs groups

---
+++ database/migrations/2024_05_28_113659_create_qlikconfs_groups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('qlikconfs_groups', function (Blueprint $table) {
+        $table->increments('id');
+        $table->unsignedBigInteger('qlik_confs_id');
+        $table->unsignedInteger('group_id');
+        $table->unique(['qlik_confs_id', 'group_id'], 'unique');
+        $table->foreign('qlik_confs_id')->references('id')->on('qlik_confs')->onDelete('cascade');
+        $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('qlikconfs_groups');
+    }
+};
