1bf1c4c3f994a92e6dae98c94b70a063ca46d9a0
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-05-28 10:56:35 +0200

    Update CBSeeder.php

--- database/seeds/CBSeeder.php
+++ database/seeds/CBSeeder.php
@@ -43,7 +43,6 @@ $this->call('QlikSett');
 
 
 
-$this->call('QlikConf');
 
         $this->command->info('Updating the data completed !');
     }
@@ -1003,35 +1002,3 @@ class QlikSett extends Seeder
     }
 }
 
-class QlikConf extends Seeder
-{
-    public function run()
-    {
-
- $mod = [
-                'created_at' => date('Y-m-d H:i:s'),
-                'name' => 'Qlik Configuration',
-                'icon' => 'fa fa-cog',
-                'path' => 'qlik_confs',
-                'table_name' => 'qlik_confs',
-                'controller' => 'QlikConfController',
-                'is_protected' => 1,
-                'is_active' => 1,
-            ];
-        DB::table('cms_moduls')->insert($mod);   
-    }
-}
-
-/*
- $mod = [
-                'created_at' => date('Y-m-d H:i:s'),
-                'name' => 'Qlik Configuration',
-                'icon' => 'fa fa-cog',
-                'path' => 'qlik_confs',
-                'table_name' => 'qlik_confs',
-                'controller' => 'QlikConfController',
-                'is_protected' => 1,
-                'is_active' => 1,
-            ];
-        DB::table('cms_moduls')->insert($mod);   
-*/
