5c122532d74393070ecaf575436922e890e38ef4
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-04 12:46:58 +0200

    tenant path

--- database/migrations/2024_06_04_124420_add_column_to_tenants_table.php
+++ database/migrations/2024_06_04_124420_add_column_to_tenants_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::table('tenants', function (Blueprint $table) {
             //
+$table->string('tenant_path')->nullable();
         });
     }
 
