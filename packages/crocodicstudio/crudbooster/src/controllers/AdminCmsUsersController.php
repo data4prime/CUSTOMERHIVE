b8dda4cb592471e25298bcb743348ffa9cb022af
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-04 08:38:08 +0200

    debugging utenze qlik

--- packages/crocodicstudio/crudbooster/src/controllers/AdminCmsUsersController.php
+++ packages/crocodicstudio/crudbooster/src/controllers/AdminCmsUsersController.php
@@ -313,31 +313,32 @@ row.parentNode.insertBefore(newColumn, row.nextSibling);
 	public static function prepare_qlik_users() {
 		//dd(Request::all());
 		if (isset(Request::all()['utenzeqlik-qlik_conf_id'])) {
-		$qlik_conf_ids = Request::all()['utenzeqlik-qlik_conf_id'];
-		$qlik_logins = Request::all()['utenzeqlik-qlik_login'];
-		$qlik_user_directory = Request::all()['utenzeqlik-user_directory'];
-		$qlik_idp_qlik = Request::all()['utenzeqlik-idp_qlik'];
+			$qlik_conf_ids = Request::all()['utenzeqlik-qlik_conf_id'];
+			$qlik_logins = Request::all()['utenzeqlik-qlik_login'];
+			$qlik_user_directory = Request::all()['utenzeqlik-user_directory'];
+			$qlik_idp_qlik = Request::all()['utenzeqlik-idp_qlik'];
 
-		$id_user = DB::table('cms_users')->select('id')->where('email',Request::all()['email'] )->first()->id;
-		$updated_idp_qlik = $qlik_idp_qlik;
+			$id_user = DB::table('cms_users')->select('id')->where('email',Request::all()['email'] )->first()->id;
+			//$updated_idp_qlik = $qlik_idp_qlik;
+			$updated_idp_qlik = [];
 
-		foreach($qlik_conf_ids as $k => $v) {
-			if (!empty($v)) {
-				if (QlikHelper::confIsSAAS($v)) {
-					if (!empty($qlik_idp_qlik[$k])) {
-						$idp = $qlik_idp_qlik[$k];
-					} else {
-						$idp = QlikHelper::createUser($id_user, $v);
+			foreach($qlik_conf_ids as $k => $v) {
+				if (!empty($v)) {
+					if (QlikHelper::confIsSAAS($v)) {
+						if (!empty($qlik_idp_qlik[$k])) {
+							$idp = $qlik_idp_qlik[$k];
+						} else {
+							$idp = QlikHelper::createUser($id_user, $v);
+						}
+						
+						//file_put_contents(__DIR__."/log.txt", $idp."\n\n", FILE_APPEND);
+						$updated_idp_qlik[$k] = $idp;
+						//Request::merge(['utenzeqlik-idp_qlik' => $updated_idp_qlik]);
+						//Request::all()['utenzeqlik-idp_qlik'][$k] = $idp;
+						Request::merge(['utenzeqlik-idp_qlik' => $updated_idp_qlik]);
 					}
-					
-					//file_put_contents(__DIR__."/log.txt", $idp."\n\n", FILE_APPEND);
-					$updated_idp_qlik[$k] = $idp;
-					//Request::merge(['utenzeqlik-idp_qlik' => $updated_idp_qlik]);
-					//Request::all()['utenzeqlik-idp_qlik'][$k] = $idp;
-					Request::merge(['utenzeqlik-idp_qlik' => $updated_idp_qlik]);
 				}
 			}
-		}
 		}
 
 		//dd(Request::all());
