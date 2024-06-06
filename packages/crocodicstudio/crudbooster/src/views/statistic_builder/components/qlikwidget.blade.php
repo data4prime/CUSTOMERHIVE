c9129f99057c8fabfdf79ac0fc3f82a763e62c3e
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-06 11:23:34 +0200

    debug mashup qlik

--- packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/qlikwidget.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/qlikwidget.blade.php
@@ -22,11 +22,12 @@
 			};
 						const baseUrl = ( config.isSecure ? 'https://' : 'http://' ) + config.host + (config.port ? ':' + config.port : '') + config.prefix;
                 console.log(baseUrl);
+/*
 				require.config({
 						baseUrl: baseUrl + 'resources',
 						webIntegrationId: config.webIntegrationId			
 			});
-/*
+
 
 			require( ["js/qlik"], function ( qlik ) {
 
