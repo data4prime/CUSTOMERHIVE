df5e6d1619083073fb587cd4dacdf02912df4ccb
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-06 18:02:58 +0200

    debug mashup qlik

--- packages/crocodicstudio/crudbooster/src/views/admin_template_plugins.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/admin_template_plugins.blade.php
@@ -14,7 +14,7 @@
 <script src="{{ asset ('vendor/crudbooster/assets/adminlte/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
 
 
- Bootstrap 3.4.1 JS -->
+ <!--Bootstrap 3.4.1 JS -->
 <script src="{{ asset ('vendor/crudbooster/assets/adminlte/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset ('vendor/crudbooster/assets/adminlte/dist/js/app.js') }}" type="text/javascript"></script>
