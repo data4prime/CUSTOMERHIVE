54437c709f1f0b172c78c0418c3f1b80c29e5509
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-06 09:56:34 +0200

    debug mashup qlik

--- packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/qlikwidget.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/qlikwidget.blade.php
@@ -21,6 +21,7 @@
                     }
 			};
 						const baseUrl = ( config.isSecure ? 'https://' : 'http://' ) + config.host + (config.port ? ':' + config.port : '') + config.prefix;
+                console.log(baseUrl);
 				require.config({
 						baseUrl: baseUrl + 'resources',
 						webIntegrationId: config.webIntegrationId// only needed in SaaS editions				
