807631f8ce5b41897bc45d448adf9f08a68aa98b
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-07 15:47:06 +0200

    qlik widget

--- packages/crocodicstudio/crudbooster/src/views/admin_template_plugins.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/admin_template_plugins.blade.php
@@ -79,7 +79,7 @@ $token = HelpersQlikHelper::getJWTToken(1, 3);
 
 
 <script type="text/javascript"  src="https://data4primesaas.eu.qlikcloud.com/resources/assets/external/requirejs/require.js"></script>
-<script  src="{{asset('js/qliksaas_login_widget.js')}}"></script> 
+<script  src="{{asset('js/qlik_login_widget.js')}}"></script> 
 <script defer type="text/javascript" >
 
 var selState;
