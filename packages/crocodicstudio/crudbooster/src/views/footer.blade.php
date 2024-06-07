2bf60953ea14e49f4d22f34886ceccdbc1c24659
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-07 09:51:58 +0200

    qlik widget

--- packages/crocodicstudio/crudbooster/src/views/footer.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/footer.blade.php
@@ -9,3 +9,52 @@
     <!-- Default to the left -->
     <strong>{{ trans('crudbooster.copyright') }} &copy; <?php echo date('Y') ?>. {{ trans('crudbooster.all_rights_reserved') }} .</strong>
 </footer>
+<script type="text/javascript"  src="https://data4primesaas.eu.qlikcloud.com/resources/assets/external/requirejs/require.js"></script>
+  <script type="text/javascript" >
+			var selState;
+			var query;
+			var filters;
+			var config = {
+				host: "data4primesaas.eu.qlikcloud.com", 
+				prefix: "/", 
+				port: 443, 
+				isSecure: true,
+				webIntegrationId: '9G9Lt4S--4o5Vj5BLq4HGEqVRpvP_Djj',
+                /*paths: {
+                    "qlik": "js/qlik"
+
+                    }*/
+			};
+						const baseUrl = ( config.isSecure ? 'https://' : 'http://' ) + config.host + (config.port ? ':' + config.port : '') + config.prefix;
+                console.log(baseUrl);
+
+				require.config({
+						baseUrl: baseUrl + 'resources',
+						webIntegrationId: config.webIntegrationId			
+			});
+
+            try {
+
+			require( ["js/qlik"], function ( qlik ) {
+                if (!qlik) {
+                        console.error("Il modulo qlik non è stato caricato correttamente.");
+                        return;
+                    }
+                qlik.setOnError( function (error){
+                        alert(error.message);
+                    });
+
+				//qlik.setLanguage("en");
+
+				var app = qlik.openApp('5a174d39-0d26-4871-bbe9-583252deaeb2', config);
+                console.log("DOPOO OPEN APP");
+				
+
+
+				
+			});
+} catch (e) {
+        console.error("Errore durante l'apertura dell'app:", e);
+    }
+
+  </script>
