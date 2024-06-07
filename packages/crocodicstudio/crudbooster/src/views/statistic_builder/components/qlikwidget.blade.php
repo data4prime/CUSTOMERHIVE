5f582269aaa26d69dc2dc27dd6ac29ac2560d94c
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-07 10:56:35 +0200

    qlik widget

--- packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/qlikwidget.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/qlikwidget.blade.php
@@ -25,6 +25,52 @@
                 class='fa fa-trash'></i></a>
     </div>-->
 </div>
+<script type="text/javascript"  src="https://data4primesaas.eu.qlikcloud.com/resources/assets/external/requirejs/require.js"></script>
+  <script type="text/javascript" >
+			var selState;
+			var query;
+			var filters;
+			var config = {
+				host: "data4primesaas.eu.qlikcloud.com", 
+				prefix: "/", 
+				port: 443, 
+				isSecure: true,
+				webIntegrationId: '9G9Lt4S--4o5Vj5BLq4HGEqVRpvP_Djj',
+
+			};
+						const baseUrl = ( config.isSecure ? 'https://' : 'http://' ) + config.host + (config.port ? ':' + config.port : '') + config.prefix;
+                console.log(baseUrl);
+
+				require.config({
+						baseUrl: baseUrl + 'resources',
+						webIntegrationId: config.webIntegrationId			
+			});
+
+            try {
+
+			require( ["js/qlik"], function ( qlik ) {
+                if (!qlik) {
+                        console.error("Il modulo qlik non è stato caricato correttamente.");
+                        return;
+                    }
+                qlik.setOnError( function (error){
+                        alert(error.message);
+                    });
+
+
+
+				var app = qlik.openApp('5a174d39-0d26-4871-bbe9-583252deaeb2', config);
+                console.log("DOPOO OPEN APP");
+				
+
+
+				
+			});
+} catch (e) {
+        console.error("Errore durante l'apertura dell'app:", e);
+    }
+
+  </script>
 @elseif($command=='configuration')
 <form method='post'>
     <input type='hidden' name='_token' value='{{csrf_token()}}' />
