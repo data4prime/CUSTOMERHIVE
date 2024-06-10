aa4e03091cf3a56fc07572d1dd7005a600525889
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-10 11:10:40 +0200

    qlik widget

--- packages/crocodicstudio/crudbooster/src/controllers/StatisticBuilderController.php
+++ packages/crocodicstudio/crudbooster/src/controllers/StatisticBuilderController.php
@@ -3,7 +3,7 @@
 namespace crocodicstudio\crudbooster\controllers;
 
 use CRUDBooster;
-use QlikMashupController;
+use \crocodicstudio\crudbooster\controllers\QlikMashupController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Excel;
 use Illuminate\Support\Facades\PDF;
