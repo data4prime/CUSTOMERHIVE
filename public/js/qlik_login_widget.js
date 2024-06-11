ba403536737937218e9fef3112f8271424382b92
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-11 19:02:21 +0200

    qlik widget

--- public/js/qlik_login_widget.js
+++ public/js/qlik_login_widget.js
@@ -70,20 +70,33 @@ if (host.includes("https://") || host.includes("http://")) {
 				var app = qlik.openApp(appId, config);
                 console.log(app);
 
-                app.getAppObjectList(function(reply) {
-                    var container = document.getElementById(appId);
-                    reply.qAppObjectList.qItems.forEach(function(item) {
-                            console.log(item.qInfo);
-                        // Crea un nuovo div per ogni oggetto
-                        var div = document.createElement('div');
-                        div.setAttribute('id', item.qInfo.qId);
-                        div.setAttribute('style', 'width: 100%; height: 400px; margin-bottom: 20px; border: 1px solid #ccc;');
-                        container.appendChild(div);
+                app.getAppObjectList('sheet', function(reply) {
+                var container = document.getElementById(appId);
+                reply.qAppObjectList.qItems.forEach(function(sheet) {
+                    // Creare un contenitore per ogni sheet
+                    var sheetDiv = document.createElement('div');
+                    sheetDiv.setAttribute('id', 'sheet-' + sheet.qInfo.qId);
+                    sheetDiv.setAttribute('style', 'margin-bottom: 40px;');
+                    container.appendChild(sheetDiv);
 
-                        // Carica l'oggetto nel div creato
-                        app.getObject(div.id, item.qInfo.qId);
+                    // Ottenere tutti gli oggetti della sheet
+                    app.getObjectProperties(sheet.qInfo.qId).then(function(properties) {
+                        var objects = properties.qChildren;
+                        objects.forEach(function(object) {
+                            // Creare un contenitore per ogni oggetto
+                            var objDiv = document.createElement('div');
+                            objDiv.setAttribute('id', object.qInfo.qId);
+                            objDiv.setAttribute('style', 'width: 100%; height: 400px; margin-bottom: 20px; border: 1px solid #ccc;');
+                            sheetDiv.appendChild(objDiv);
+
+                            // Caricare l'oggetto nel contenitore
+                            loadObject(app, object.qInfo.qId, object.qInfo.qId);
+                        });
+                    }).catch(function(error) {
+                        console.error("Errore nel recupero delle proprietà della sheet:", sheet.qInfo.qId, error);
                     });
                 });
+            });
 
             //getSheets();
            // getAppDetails();
