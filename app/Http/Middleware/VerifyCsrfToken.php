c7b2e3386b144e35f5cc80d65b1848b85524586c
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-11 11:36:14 +0200

    qlik widget

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends BaseVerifier
      */
     protected $except = [
         //
-        '/admin/statistic_builder/*'
+        '/admin/statistic_builder/*',
+        '/mashup/*',
     ];
 }
