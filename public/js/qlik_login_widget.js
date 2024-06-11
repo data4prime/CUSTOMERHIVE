ce217a45de61a11123f6a918f09a58dd6787b894
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-11 17:26:56 +0200

    qlik widget

--- public/js/qlik_login_widget.js
+++ public/js/qlik_login_widget.js
@@ -71,6 +71,51 @@ if (host.includes("https://") || host.includes("http://")) {
                 console.log(app);
 
 
+function getSheets(){
+	app.getAppObjectList( 'sheet', function(reply){
+	var str = "";
+	console.log(reply);
+	$.each(reply.qAppObjectList.qItems, function(key, value) {
+		console.log(value.qName);
+		str=str+ "<br>" + value.qData.title + "    -    " + value.qInfo.qId;
+		app.getFullPropertyTree(value.qInfo.qId).then(function(reply){
+		});	
+	});
+	//$('#QV01').html(str)
+	});
+	}
+	
+	function getAppDetails(){
+	app.getAppLayout(function(layout){
+	console.log("Layout")
+	console.log(layout);
+	//$('#QV01').html(JSON.stringify(layout))
+	});
+	}
+	function getList(listType){
+	app.getList(listType, function(reply){	
+		var str="";
+		$.each(reply["q"+listType].qItems, function(key, value) {
+			
+			str=str+ "<br>" + value.qData.title + "    -    " + value.qInfo.qId;
+			});
+		//$('#QV01').html(str);
+		});
+	}
+	function getVariables(){
+	var str="";
+	app.getList("VariableList", function(reply){
+	$.each(reply.qVariableList.qItems, function(key, value) {
+		str=str+ "<br>" + value.qName + "    -    " + value.qInfo.qId;;
+		app.variable.getContent(value.qName).then(function(model){
+   		console.log(model);
+		});
+	});
+//$('#QV01').html(str);
+	});
+	}
+
+
 
 				
 			});
