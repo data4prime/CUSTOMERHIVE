5961bb08d68570c83995a6090b53e460711ee9b1
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-11 14:25:02 +0200

    qlik widget

--- packages/crocodicstudio/crudbooster/src/views/sidebar.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/sidebar.blade.php
@@ -152,6 +152,15 @@
 
         </li>
 
+        <li data-collapse="3" class='treeview'>
+          <a href='{{url("admin/qlik_mashups")}}'>
+            <img class="menu qlik_logo" src=/images/qlik_logo.png />
+            <span>{{ trans('crudbooster.Qlik_Mashups') }}</span> <i class="fa fa-angle-{{ trans("
+              crudbooster.right") }} pull-{{ trans("crudbooster.right") }}"></i>
+          </a>
+
+        </li>
+
         @if(CRUDBooster::isSuperadmin())
         <li data-collapse="3" class="treeview">
           <a href="#"><i class='fa fa-wrench'></i> <span>{{ trans('crudbooster.settings') }}</span> <i
