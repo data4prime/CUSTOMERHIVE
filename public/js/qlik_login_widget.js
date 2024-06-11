2e1377cf7c3251048a898a57f2008c7ec67f8a1b
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-11 14:41:20 +0200

    qlik widget

--- public/js/qlik_login_widget.js
+++ public/js/qlik_login_widget.js
@@ -33,8 +33,8 @@ if (host.includes("https://") || host.includes("http://")) {
 				host: "sense.izsvenezie.it", //the address of your Qlik Engine Instance
 				prefix: "/pub", //or the virtual proxy to be used. for example "/anonymous/"
 				port: 443, //or the port to be used if different from the default port  
-				isSecure: true //should be true if connecting over HTTPS
-				//webIntegrationId: 'web-integration-id-here' //only needed in SaaS editions
+				isSecure: true, //should be true if connecting over HTTPS
+				webIntegrationId: webIntegrationId //only needed in SaaS editions
 			};
 						const baseUrl = ( config.isSecure ? 'https://' : 'http://' ) + config.host + (config.port ? ':' + config.port : '') + config.prefix;
                 //console.log(baseUrl);
