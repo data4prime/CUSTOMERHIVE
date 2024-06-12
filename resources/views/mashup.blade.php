deb20729322e81c1f9b6f30afc884521ae32ce3a
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-12 17:29:52 +0200

    qlik widget

--- resources/views/mashup.blade.php
+++ resources/views/mashup.blade.php
@@ -11,7 +11,7 @@ $token = HelpersQlikHelper::getJWTToken(1, 3);
 
 $conf = QlikMashupController::getConf($qlik_conf);
 
-dd(gettype$conf);
+dd(gettype($conf));
 
 @endphp 
 @if (isset($conf) || !empty($conf)) 
