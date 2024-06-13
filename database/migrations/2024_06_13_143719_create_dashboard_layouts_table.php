6166c60cc27e47e976a7f00ff10c7b8d4341e1df
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-13 16:50:46 +0200

    dashboard layout

--- database/migrations/2024_06_13_143719_create_dashboard_layouts_table.php
+++ database/migrations/2024_06_13_143719_create_dashboard_layouts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('dashboard_layouts', function (Blueprint $table) {
             $table->id();
+            $table->string('layoutname');
+            $table->text('code_layout');
+
             $table->timestamps();
         });
     }
