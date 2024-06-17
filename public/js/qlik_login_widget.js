3c754101306d3b95ef02a2d4db7a8fef1cb82a98
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-17 17:23:56 +0200

    qlik object select

--- public/js/qlik_login_widget.js
+++ public/js/qlik_login_widget.js
@@ -75,6 +75,7 @@ function objectsOptions(app) {
             sheetDiv.className = 'masterobject-option';
             sheetDiv.value = sheetId;
             sheetDiv.innerHTML = name + ' (' + sheetId + ')';
+			console.log("mashup_object");
 			console.log(parent.document.getElementById('mashup_object'));
 
 
