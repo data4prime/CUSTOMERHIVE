5759d978aea31bb0f2756ab3c596d7152204ce77
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-17 10:58:03 +0200

    qlik object select

--- packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/qlikwidget.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/qlikwidget.blade.php
@@ -36,63 +36,7 @@ use crocodicstudio\crudbooster\controllers\QlikMashupController;
     </div>
  @elseif($command=='configuration')
 
-<div id='{{$componentID}}'>
-<iframe src="/mashup-objects/{{$mashup}}/{{$componentID}}" id="configuration" frameborder="0" style="width: 100%;height: 80%;"></iframe>
-</div>
 
-
-<script defer >
-
-
-
-function create_object_select(select){
-    var mashup_id = $(select).val();
-
-    console.log(mashup_id);
-
-    document.getElementById('configuration').src = '/mashup-objects/'+mashup_id+'/'+$componentID;
-
-
-
-    
-}
-
-</script>
-
-
-
-<form method='post'>
-    <input type='hidden' name='_token' value='{{csrf_token()}}' />
-    <input type='hidden' name='componentid' value='{{$componentID}}' />
-    <div class="form-group">
-     
-
-
-    <div class="form-group">
-        <label>Mashup</label>
-        <select onchange="create_object_select(this)" class='form-control' required name='config[mashups]'>
-            <option value='0'>Choose Mashup</option>
-            @foreach($mashups as $m)
-            @if(isset($config) && $m->id == $config->mashups)
-            <option selected value='{{$m->id}}'>{{$m->mashupname}}</option>
-            @else
-            <option  value='{{$m->id}}'>{{$m->mashupname}}</option>
-            @endif
-            @endforeach
-        </select>
-    </div>
-
-<div class="form-group">
-        <label>Mashup</label>
-        <select  class='form-control' required name='config[object]'>
-            <option value='0'>Choose Object</option>
-
-        </select>
-    </div>
-
-
-
-</form>
 @elseif($command=='showFunction')
 <?php
     if ($key == 'sql') {
