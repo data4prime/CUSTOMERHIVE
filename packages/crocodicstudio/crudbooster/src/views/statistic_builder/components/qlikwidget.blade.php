666642f03796e1b076ba1af21115ffc0c5b56a38
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-06-26 18:03:40 +0200

    Qlik Widgets Connection

--- packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/qlikwidget.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/qlikwidget.blade.php
@@ -42,7 +42,8 @@ use crocodicstudio\crudbooster\helpers\CRUDBooster;
 @if ($mashup->id != 0)
 <iframe src="/mashup/{{$componentID}}" frameborder="0" style="width: 100%;height: 80%;"></iframe>
 @else 
-Configurate the widget from Statistic Builder
+<p>Configurate the widget<br>from Statistic Builder</p>
+
 <img style="width: 20%;" src='/images/qlik_logo.png' />
 @endif 
 
