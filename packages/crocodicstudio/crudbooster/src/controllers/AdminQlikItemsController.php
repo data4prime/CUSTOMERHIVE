358cc65930762ebb423fbfb4d668f7951c9fe8f1
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-07-01 15:44:47 +0200

    Qlik Items ONPREMISE

--- packages/crocodicstudio/crudbooster/src/controllers/AdminQlikItemsController.php
+++ packages/crocodicstudio/crudbooster/src/controllers/AdminQlikItemsController.php
@@ -448,7 +448,6 @@ class AdminQlikItemsController extends CBController
 			CRUDBooster::redirect(CRUDBooster::adminPath(), trans("crudbooster.missing_item"));
 		}
 		$conf = QlikHelper::getConfFromItem($qlik_item_id);
-		dd($conf);
 		$type = $conf->type;
 		//$type = CRUDBooster::getSetting('type');
 		//add menu settings
