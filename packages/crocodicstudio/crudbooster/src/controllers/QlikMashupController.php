ff3b6c06ab7f2d908e382e9cd3486c71bf8ef60b
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-07-02 12:10:32 +0200

    Qlik Mashup On-Premise

--- packages/crocodicstudio/crudbooster/src/controllers/QlikMashupController.php
+++ packages/crocodicstudio/crudbooster/src/controllers/QlikMashupController.php
@@ -305,7 +305,7 @@ class QlikMashupController extends CBController
 
 	public static function getConf($id) {
 		$qlik_conf = DB::table('qlik_confs')->where('id', $id)->first();
-		//dd($id);
+		dd($id);
 		//dd($qlik_conf);
 		$return = [];
 
