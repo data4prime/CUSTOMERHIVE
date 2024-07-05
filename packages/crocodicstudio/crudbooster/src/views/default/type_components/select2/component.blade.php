5e26b589d3af3b5f2b84c9b7845fb0786337b24b
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-07-05 17:44:14 +0200

    foreign keys

--- packages/crocodicstudio/crudbooster/src/views/default/type_components/select2/component.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/default/type_components/select2/component.blade.php
@@ -6,7 +6,7 @@ $foreignKey2 = CRUDBooster::getForeignKey($table_name, $form['relationship_table
 $value = DB::table($form['relationship_table'])->where($foreignKey, (isset($id) ? $id : 0))->toSql();
 //$value = $value->pluck($foreignKey2)->toArray();
 
-dd($value);
+dd($id);
 @endphp
 
 @if(isset($form['datatable']))
