84fbf6ac88260016cdf3a7ff90e41a44f7041bbe
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-07-10 13:10:24 +0200

    Fixing bugs

--- packages/crocodicstudio/crudbooster/src/views/forgot.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/forgot.blade.php
@@ -50,8 +50,7 @@
     <div class="login-box">
         <div class="login-logo">
             <a href="{{url('/')}}">
-                <img title='{!!(isset($appname))? $appname : "<b>CRUD</b>Booster"!!}' src='{{
-                CRUDBooster::getSetting("logo")?asset(CRUDBooster::getSetting("logo")):asset("vendor/crudbooster/assets/logo_crudbooster.png") }}'
+                <img title='{!!(isset($appname))? $appname : "<b>CRUD</b>Booster"!!}' src='{{ $logo }}'
                     style='max-width: 100%;max-height:170px' />
             </a>
         </div><!-- /.login-logo -->
