9e1132e4c64f34a351231c7617fc40a37787a60a
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-07-12 15:40:20 +0200

    Mass Edit

--- packages/crocodicstudio/crudbooster/src/views/mass_edit/form_body.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/mass_edit/form_body.blade.php
@@ -22,11 +22,14 @@
     @endphp
 
     @if(!in_array($type, $asset_already))
+      <div class="row">
         @if(file_exists(base_path('/packages/crocodicstudio/crudbooster/src/views/default/type_components/'.$type.'/asset.blade.php')))
             @include('crudbooster::default.type_components.'.$type.'.asset')
         @elseif(file_exists(resource_path('views/vendor/crudbooster/type_components/'.$type.'/asset.blade.php')))
             @include('vendor.crudbooster.type_components.'.$type.'.asset')
-        @endif
+        @endif      
+      </div>
+
 
         @php
             $asset_already[] = $type;
