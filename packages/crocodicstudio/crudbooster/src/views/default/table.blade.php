4593784f412e22eaeed24e3c9d01c2bec733f852
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-07-12 12:32:29 +0200

    Mass Edit

--- packages/crocodicstudio/crudbooster/src/views/default/table.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/default/table.blade.php
@@ -630,11 +630,11 @@ $('#mass_editing_button').click(function () {
                     <span aria-hidden="true">×</span></button>
                 <h4 class="modal-title"><i class='fa fa-pencil'></i> Mass Edit</h4>
             </div>
-            <form method='post' action='{{ CRUDBooster::mainpath("mass-edit") }}' id='form-mass-editing'>
+            <form class="modal-body" method='post' action='{{ CRUDBooster::mainpath("mass-edit") }}' id='form-mass-editing'>
                 
             <input type='hidden' name='_token' value='{{csrf_token()}}' />
             <input type='hidden' name='table' value='{{$table}}' />
-            <div class="modal-body">@include("crudbooster::mass_edit.form_body")</div>
+            @include("crudbooster::mass_edit.form_body")
 
             <div class="modal-footer" align="right">
                     <button class="btn btn-default" type="button"
