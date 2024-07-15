9038bb8c1da1d635e1024ba32190a6c7474ec76b
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-07-15 17:17:51 +0200

    Fix MultiText Component

--- packages/crocodicstudio/crudbooster/src/views/default/type_components/checkbox/component.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/default/type_components/checkbox/component.blade.php
@@ -2,16 +2,21 @@
 <div class='form-group {{$header_group_class}} {{ ($errors->first($name))?"has-error":"" }}' 
 id='form-group-{{$name}}' style="{!! @$form['style'] !!}">
   <label class='control-label col-sm-2'>{{$form['label']}}
-    @if($required)
+    @if(isset($required))
     <span class='text-danger' title="{!! trans('crudbooster.this_field_is_required') !!}" >*</span>
     @endif
   </label>
-  <div class="{{$col_width?:'col-sm-10'}}">
+  <div class="{{ isset($col_width)? $col_width:'col-sm-10'}}">
 
     <?php
-        if(isset($form['dataenum']) && $form['dataenum']=='' AND !@$form['datatable'] AND !$form['dataquery']) {
+        $single = !isset($form['dataenum']) && !isset($form['datatable']) && !isset($form['dataquery']);
+        //if(isset($form['dataenum']) && $form['dataenum']=='' AND !@$form['datatable'] AND !$form['dataquery']) {
           //se non ci sono altre etichette possibili attiva una versione semplice di checkbox
-          $checked = (!empty($checked) OR !empty($value)) ? "checked" : "";
+          //$checked = (!empty($checked) OR !empty($value)) ? "checked" : "";
+          if ($single) {
+            $checked = (!empty($checked) OR !empty($value)) ? "checked" : "";
+
+          }
           ?>
     <div class="checkbox {{$disabled}}">
       <label>
