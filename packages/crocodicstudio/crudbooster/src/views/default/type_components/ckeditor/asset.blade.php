9e4f386e73d003483e6df7ffb89ca30c82389053
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-07-22 12:33:05 +0200

    adding ckeditor

--- packages/crocodicstudio/crudbooster/src/views/default/type_components/ckeditor/asset.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/default/type_components/ckeditor/asset.blade.php
@@ -1,6 +1,6 @@
 @push('head')
-  <link rel='stylesheet' href="{{asset('ckeditor/ckeditor5.css')}}" type="text/css"/>
+  <link rel='stylesheet' href="{{asset('ckeditor5/ckeditor5.css')}}" type="text/css"/>
 @endpush
 @push('bottom')
-    <script type="text/javascript" src="{{asset('ckeditor/ckeditor5.js')}}"></script>
+    <script type="text/javascript" src="{{asset('ckeditor5/ckeditor5.js')}}"></script>
 @endpush
