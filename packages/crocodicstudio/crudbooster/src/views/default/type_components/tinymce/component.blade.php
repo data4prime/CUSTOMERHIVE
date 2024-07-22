25fa23da358da1027b49f1ce6683f5c77c29bb9f
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-07-22 16:39:33 +0200

    adding tinymce

--- packages/crocodicstudio/crudbooster/src/views/default/type_components/tinymce/component.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/default/type_components/tinymce/component.blade.php
@@ -3,7 +3,7 @@
     <label class='control-label col-sm-2'>{{$form['label']}}</label>
 
     <div class="{{$col_width?:'col-sm-10'}}">
-        <textarea id="myeditorinstance{{$name}}" {{$required}} {{$readonly}} {{$disabled}} name="{{$form['name']}}"></textarea>
+        <textarea id="myeditorinstance{{$form['name']}}" {{$required}} {{$readonly}} {{$disabled}} name="{{$form['name']}}"></textarea>
         <div class="text-danger">{{ $errors->first($name) }}</div>
         <p class='help-block'>{{ @$form['help'] }}</p>
     </div>
