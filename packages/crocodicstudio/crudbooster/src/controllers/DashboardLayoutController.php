698034fdf37e3fd24cacbe39880948d3bc50749f
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-07-23 08:47:05 +0200

    adding tinymce

--- packages/crocodicstudio/crudbooster/src/controllers/DashboardLayoutController.php
+++ packages/crocodicstudio/crudbooster/src/controllers/DashboardLayoutController.php
@@ -295,6 +295,7 @@ $this->form[] = ['label' => 'Code Layout', 'name' => 'code_layout', 'type' => 't
 		//dd($postdata);
 		$postdata['code_layout'] = $this->aggiungiIdAElemTd($postdata['code_layout']);
 		$postdata['code_layout'] = $this->aggiungiClassAElemTd($postdata['code_layout']);
+		dd($postdata);
 		//$postdata['code_layout'] = htmlentities($postdata['code_layout']);
 		//$postdata['code_layout'] = str_replace("\r\n", "", $postdata['code_layout']);
 		
