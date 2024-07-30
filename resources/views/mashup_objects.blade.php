18aaacb22fe071af53b0dc464f2114c1031ed632
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-07-30 12:37:39 +0200

    Qlik On Premise fixing

--- resources/views/mashup_objects.blade.php
+++ resources/views/mashup_objects.blade.php
@@ -45,7 +45,7 @@ $param = $conf->type == 'On-Premise' ? "?qlikTicket=$token" : "";
 </head>
 
 <body>
-<img src="https://qse.datasynapsi.cloud/resources/hub/img/core/logo/Qlik-Logo_RGB.svg?qlikTicket={{$token3}}" alt="Qlik Logo" style="width: 200px; height: 100px;">
+<!--<img src="https://qse.datasynapsi.cloud/resources/hub/img/core/logo/Qlik-Logo_RGB.svg?qlikTicket={{$token3}}" alt="Qlik Logo" style="width: 200px; height: 100px;">-->
   <div id="{{$mashup->appid}}" >
 <script  type="text/javascript" >
 
