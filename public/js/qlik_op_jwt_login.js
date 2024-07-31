a460f053529b3cbfceac5805898ed48531b39aff
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-07-31 15:27:36 +0200

    Qlik On Premise fixing

--- public/js/qlik_op_jwt_login.js
+++ public/js/qlik_op_jwt_login.js
@@ -63,8 +63,8 @@ async function jwtLogin(token) {
     console.log(authHeader);
     //console.log(WEBINTEGRATIONID);
     return await fetch(`${TENANT}/qrs/about?xrfkey=0123456789abcdef`, {
-        //credentials: 'include',
-        mode: 'no-cors',
+        credentials: 'include',
+        mode: 'cors',
         method: 'GET',
         headers: {
             'Authorization': authHeader,
