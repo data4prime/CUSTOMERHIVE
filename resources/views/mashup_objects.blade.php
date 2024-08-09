fac9a582eabb0f7000072a4997b2e887252b3e3b
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-08-09 17:32:25 +0200

    Qlik JS defer

--- resources/views/mashup_objects.blade.php
+++ resources/views/mashup_objects.blade.php
@@ -124,7 +124,7 @@ var hidden_app = parent.document.getElementById('mashup_app_hidden');
 </script>
 <script  src="{{asset($js_defer)}}.js"></script>
 <script type="text/javascript" defer  src="{{$src}}"></script>
-<script  src="{{asset($js_defer)}}2.js"></script>
+<script defer  src="{{asset($js_defer)}}2.js"></script>
 
 
 <link rel="stylesheet" href="{{ $css }}">
