123c392e5523af4a90ad5e818cffc4d6e519cc63
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-08-19 16:35:02 +0200

    qlik op jwt

--- resources/views/mashup_objects.blade.php
+++ resources/views/mashup_objects.blade.php
@@ -146,7 +146,8 @@ var hidden_app = parent.document.getElementById('mashup_app_hidden');
 
 @endif
 </body>
-<script type="text/javascript" defer  src="{{$src}}"></script>
+
+<!--<script type="text/javascript" defer  src="{{$src}}"></script>-->
 
 
 <script defer type="text/javascript"  src="{{asset($js_defer)}}2.js"></script>
