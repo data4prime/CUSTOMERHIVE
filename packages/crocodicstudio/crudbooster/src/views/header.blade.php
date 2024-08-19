c5da701f24d659b2d401bd9492e57f9a1b7c4384
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-08-19 16:03:07 +0200

    chat header

--- packages/crocodicstudio/crudbooster/src/views/header.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/header.blade.php
@@ -14,10 +14,10 @@
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
 
-<li class="dropdown notifications-menu">
+                <li class="dropdown notifications-menu">
 
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" title='Notifications' aria-expanded="false">
-                        <i id='icon_notification' class="fa fa-bell-o"></i>
+                        <i id='icon_notification' class="fa fa-robot-o"></i>
                         <span id='notification_count' class="label label-danger" style="display:none">0</span>
                     </a>
                     <ul id='list_notifications' class="dropdown-menu">
@@ -25,21 +25,14 @@
                         <li>
                             <!-- inner menu: contains the actual data -->
                             <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 200px;">
-                                <ul class="menu" style="overflow: hidden; width: 100%; height: 200px;">
-                                    <li>
-                                        <a href="#">
-                                            <em>{{trans("crudbooster.text_no_notification")}}</em>
-                                        </a>
-                                    </li>
-
-                                </ul>
+                                
                                 <div class="slimScrollBar"
                                      style="width: 3px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 195.122px; background: rgb(0, 0, 0);"></div>
                                 <div class="slimScrollRail"
                                      style="width: 3px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; opacity: 0.2; z-index: 90; right: 1px; background: rgb(51, 51, 51);"></div>
                             </div>
                         </li>
-                        <li class="footer"><a href="{{route('NotificationsControllerGetIndex')}}">{{trans("crudbooster.text_view_all_notification")}}</a></li>
+                        <!--<li class="footer"><a href="{{route('NotificationsControllerGetIndex')}}">{{trans("crudbooster.text_view_all_notification")}}</a></li>-->
                     </ul>
                 </li>
 
