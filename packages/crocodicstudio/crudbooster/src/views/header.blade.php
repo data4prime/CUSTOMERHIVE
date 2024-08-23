d2f79d789ffb48e7ac48a556290e206df34ca7a3
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-08-23 14:42:47 +0200

    Chat AI

--- packages/crocodicstudio/crudbooster/src/views/header.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/header.blade.php
@@ -20,18 +20,7 @@
                         </i>
                         <span id='assistance_count' class="label label-danger" style="display:none">0</span>
                     </a>
-                    <ul id='list_assistance' class="dropdown-menu">
-                        <li class="header"></li>
-                        <li>
-                            <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto;">
-                                @include('crudbooster::chat')
-                                <div class="slimScrollBar"
-                                     style="width: 3px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 195.122px; background: rgb(0, 0, 0);"></div>
-                                <div class="slimScrollRail"
-                                     style="width: 3px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; opacity: 0.2; z-index: 90; right: 1px; background: rgb(51, 51, 51);"></div>
-                            </div>
-                        </li>
-                    </ul>
+
                 </li>
 
                 <li class="dropdown notifications-menu">
