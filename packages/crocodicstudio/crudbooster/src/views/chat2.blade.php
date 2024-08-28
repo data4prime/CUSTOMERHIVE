8d3719ce1db0127a52913df9a80bbfac180e3613
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-08-28 12:32:41 +0200

    POST to N8N

--- packages/crocodicstudio/crudbooster/src/views/chat2.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/chat2.blade.php
@@ -387,7 +387,7 @@ button, input, optgroup, select, textarea {
 
               <div class="ps-container ps-theme-default ps-active-y" id="chat-content" style="overflow-y: scroll !important; height:550px !important;">
                 <div class="media media-chat">
-                  <img class="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="...">
+                  <img class="avatar" src="/images/user/admin.jpeg" alt="...">
                   <div class="media-body">
                     <p>Ciao</p>
                     <p>Sono il tuo assistente AI</p>
@@ -401,7 +401,7 @@ button, input, optgroup, select, textarea {
                 <div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 0px; height: 0px; right: 2px;"><div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 2px;"></div></div></div>
 
               <div class="publisher bt-1 border-light">
-                <img class="avatar avatar-xs" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="...">
+                <img class="avatar avatar-xs" src="/images/user/admin.jpeg" alt="...">
                 <input class="publisher-input" type="text" placeholder="Scrivi qui">
                 <button id="send-btn" class="publisher-btn text-info"  type="button"><i class="fa fa-paper-plane"></i></button>
               </div>
@@ -484,7 +484,7 @@ document.getElementById('send-btn').addEventListener('click', function() {
       .then((response) => response.json())
       .then((data) => {
         console.log('Success:', data);
-        var html = '<div class="media media-chat"><img class="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..."><div class="media-body"><p>'+data.message+'</p></div></div>';
+        var html = '<div class="media media-chat"><img class="avatar" src="/images/user/admin.jpeg" alt="..."><div class="media-body"><p>'+data.message+'</p></div></div>';
         document.getElementById('chat-content').innerHTML += html;
 
         //scroll to bottom chat
