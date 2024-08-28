fffb441c299102031567f4961c9c0d241c3b681f
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-08-28 16:39:28 +0200

    Chat AI draggable

--- packages/crocodicstudio/crudbooster/src/views/admin_template_plugins.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/admin_template_plugins.blade.php
@@ -6,10 +6,9 @@
 <!-- REQUIRED JS SCRIPTS -->
 
 <!-- jQuery 2.2.3 -->
-
-
-
 <script src="{{ asset ('vendor/crudbooster/assets/adminlte/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
+
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 
  <!--Bootstrap 3.4.1 JS -->
 <script src="{{ asset ('vendor/crudbooster/assets/adminlte/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
