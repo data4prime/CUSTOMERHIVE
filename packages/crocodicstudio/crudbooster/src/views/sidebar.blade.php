dca2de4165c4796b85319f34fd63906ec0b3a6c8
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-08-28 09:33:45 +0200

    Chat AI sidebar

--- packages/crocodicstudio/crudbooster/src/views/sidebar.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/sidebar.blade.php
@@ -154,8 +154,8 @@
 
         <li data-collapse="3" class='treeview'>
           <a href='{{url("admin/qlik_confs")}}'>
-            <img class="menu qlik_logo" src=/images/qlik_logo.png />
-            <span>Chat AI</span> <i class="fa fa-comments-o"></i>
+            <i class="fa fa-comments-o"></i>
+            <span>Chat AI</span> 
           </a>
 
         </li>
