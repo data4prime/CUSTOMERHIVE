9ef70cc3e3d7d05c47cd4ef52ea2a500b4b40a4b
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-08-28 17:24:51 +0200

    Chat AI draggable

--- packages/crocodicstudio/crudbooster/src/views/chat2.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/chat2.blade.php
@@ -395,13 +395,13 @@ button, input, optgroup, select, textarea {
 
 </style>
 
-
+<!--row  d-flex justify-content-center -->
 
 <aside class=" main-sidebar-right" id="draggable">
   <section class="sidebar" style="padding-bottom: 0px;">
 <div class="page-content page-container" id="page-content">
     <div>
-        <div style="margin: 0;" class="row  d-flex justify-content-center">
+        <div style="margin: 0;">
 
 <div  style="padding-left: 0px;padding-right: 0px;">
             <div class="card card-bordered">
