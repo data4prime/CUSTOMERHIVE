c61dab220bc026083f5691ecb0a5ee91c6d30bae
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-09-03 15:21:58 +0200

    Agent AI Menu

--- resources/views/chat_ai/view.blade.php
+++ resources/views/chat_ai/view.blade.php
@@ -1,13 +1,137 @@
-@php 
+@extends('crudbooster::admin_template',['target_layout' => isset($row->target_layout) ? $row->target_layout : null ])
 
-use Illuminate\Support\Facades\Session;
+@if(isset($row->target_layout) && $row->target_layout == 2)
+<!-- fill content settings -->
+@section('content')
+<div class="chat-window" id="chatWindow">
+    <div class="chat-header">
+        Chat AI
+    <div id="x-close-chatai" ><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
 
-$chat_messages = Session::get('chat_messages');
+    </div>
+    <div class="chat-body">
 
 
 
-@endphp 
 
+        <div class="media media-chat">
+                  <img class="avatar" src="/images/user/chatai.jpg" alt="...">
+                  <div class="media-body">
+                    <p>Ciao</p>
+                    <p>Sono il tuo assistente AI</p>
+                    <p>Come posso aiutarti?</p>
+                  </div>
+                </div>
+    @if ($chat_messages)
+        @foreach ($chat_messages as $chat_message)
+            <div class="media media-chat media-chat-reverse">
+                <img class="avatar" src="/images/user/admin.jpeg" alt="...">
+                <div class="media-body">
+                    <p>{{ $chat_message['message'] }}</p>
+                </div>
+            </div>
+
+            <div class="media media-chat">
+                <img class="avatar" src="/images/user/chatai.jpg" alt="...">
+                <div class="media-body">
+                    <p>{{$chat_message['response']}}</p>
+
+                </div>
+            </div>
+        @endforeach
+            
+    @endif
+    </div>
+    <div class="chat-footer">
+        <div class="chat-input">
+            <input type="text" id="publisher-input" style="margin-right: 5px ;" class="form-control" placeholder="Chiedi qualcosa...">
+            <div class="input-group-append">
+                <button id="send-btn" class="btn btn-primary" type="button">Invia</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+@else
+<!-- default -->
+@section('content')
+<div class="box qi_box">
+  <h4 class="qi_subtitle">{{ $subtitle }}</h4>
+
+  <div class="qi_iframe_container">
+    <a href="{{$debug_url}}" target="_blank">{{$debug_url}}</a>
+    <iframe class="qi_iframe" src="{{ $item_url }}"  style="border:none;"></iframe>
+  </div>
+</div>
+@endsection
+@endif
+
+@push('bottom')
+<script type="text/javascript">
+
+document.getElementById('send-btn').addEventListener('click', function(event) {
+    sendMessage(event);
+});
+
+document.getElementById('publisher-input').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        sendMessage(event);
+    }
+});
+
+function sendMessage(event) {
+    event.preventDefault();
+
+    console.log('clicked');
+
+    var message = document.querySelector('#publisher-input').value;
+    console.log(message);
+    if(message != ''){
+        var html = '<div class="media media-chat media-chat-reverse"><img class="avatar" src="/images/user/admin.jpeg" alt="..."><div class="media-body"><p>'+message+'</p></div></div>';
+        document.querySelector('.chat-body').insertAdjacentHTML('beforeend', html);
+        document.querySelector('#publisher-input').value = '';
+    }
+
+    fetch('/admin/chat_ai/send_message', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 'message': message }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Success:', data);
+        //if data.text exists
+        if(data.text){
+            var html = '<div class="media media-chat"><img class="avatar" src="/images/user/chatai.jpg" alt="..."><div class="media-body"><p>'+data.text+'</p></div></div>';
+            document.querySelector('.chat-body').insertAdjacentHTML('beforeend', html);
+        }
+
+        if(data.message){
+            var html = '<div class="media media-chat"><img class="avatar" src="/images/user/chatai.jpg" alt="..."><div class="media-body"><p>'+data.message+'</p></div></div>';
+            document.querySelector('.chat-body').insertAdjacentHTML('beforeend', html);
+        }
+
+        var objDiv = document.querySelector('.chat-body');
+        objDiv.scrollTop = objDiv.scrollHeight;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+    var objDiv = document.querySelector('.chat-body');
+    objDiv.scrollTop = objDiv.scrollHeight;
+}
+
+var objDiv = document.querySelector('.chat-body');
+objDiv.scrollTop = objDiv.scrollHeight;
+
+</script>
+@endpush
+
+@push('head')
     <style>
 
 
@@ -176,119 +300,4 @@ h4.chat-title {
 
     </style>
 
-
-
-
-<div class="chat-window" id="chatWindow">
-    <div class="chat-header">
-        Chat AI
-    <div id="x-close-chatai" ><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
-
-    </div>
-    <div class="chat-body">
-
-
-
-
-        <div class="media media-chat">
-                  <img class="avatar" src="/images/user/chatai.jpg" alt="...">
-                  <div class="media-body">
-                    <p>Ciao</p>
-                    <p>Sono il tuo assistente AI</p>
-                    <p>Come posso aiutarti?</p>
-                  </div>
-                </div>
-    @if ($chat_messages)
-        @foreach ($chat_messages as $chat_message)
-            <div class="media media-chat media-chat-reverse">
-                <img class="avatar" src="/images/user/admin.jpeg" alt="...">
-                <div class="media-body">
-                    <p>{{ $chat_message['message'] }}</p>
-                </div>
-            </div>
-
-            <div class="media media-chat">
-                <img class="avatar" src="/images/user/chatai.jpg" alt="...">
-                <div class="media-body">
-                    <p>{{$chat_message['response']}}</p>
-
-                </div>
-            </div>
-        @endforeach
-            
-    @endif
-    </div>
-    <div class="chat-footer">
-        <div class="chat-input">
-            <input type="text" id="publisher-input" style="margin-right: 5px ;" class="form-control" placeholder="Chiedi qualcosa...">
-            <div class="input-group-append">
-                <button id="send-btn" class="btn btn-primary" type="button">Invia</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-<script>
-
-document.getElementById('send-btn').addEventListener('click', function(event) {
-    sendMessage(event);
-});
-
-document.getElementById('publisher-input').addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
-        sendMessage(event);
-    }
-});
-
-function sendMessage(event) {
-    event.preventDefault();
-
-    console.log('clicked');
-
-    var message = document.querySelector('#publisher-input').value;
-    console.log(message);
-    if(message != ''){
-        var html = '<div class="media media-chat media-chat-reverse"><img class="avatar" src="/images/user/admin.jpeg" alt="..."><div class="media-body"><p>'+message+'</p></div></div>';
-        document.querySelector('.chat-body').insertAdjacentHTML('beforeend', html);
-        document.querySelector('#publisher-input').value = '';
-    }
-
-    fetch('/admin/chat_ai/send_message', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ 'message': message }),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log('Success:', data);
-        //if data.text exists
-        if(data.text){
-            var html = '<div class="media media-chat"><img class="avatar" src="/images/user/chatai.jpg" alt="..."><div class="media-body"><p>'+data.text+'</p></div></div>';
-            document.querySelector('.chat-body').insertAdjacentHTML('beforeend', html);
-        }
-
-        if(data.message){
-            var html = '<div class="media media-chat"><img class="avatar" src="/images/user/chatai.jpg" alt="..."><div class="media-body"><p>'+data.message+'</p></div></div>';
-            document.querySelector('.chat-body').insertAdjacentHTML('beforeend', html);
-        }
-
-        var objDiv = document.querySelector('.chat-body');
-        objDiv.scrollTop = objDiv.scrollHeight;
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-
-    var objDiv = document.querySelector('.chat-body');
-    objDiv.scrollTop = objDiv.scrollHeight;
-}
-
-var objDiv = document.querySelector('.chat-body');
-objDiv.scrollTop = objDiv.scrollHeight;
-
-
-</script>
+@endpush
