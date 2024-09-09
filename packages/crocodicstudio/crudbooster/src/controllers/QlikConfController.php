cebdc8f2daacc6030811291a85100ed2525c46d7
donicid4p <121858746+donicid4p@users.noreply.github.com>
2024-09-09 11:30:41 +0200

    Chat Layout

--- packages/crocodicstudio/crudbooster/src/controllers/QlikConfController.php
+++ packages/crocodicstudio/crudbooster/src/controllers/QlikConfController.php
@@ -255,7 +255,9 @@ class QlikConfController extends CBController
                     fields[key].forEach(fieldId => {
 
                         if (document.getElementsByName(fieldId)[0]) {
-                            document.getElementsByName(fieldId)[0].parentNode.parentNode.classList.add('hidden');
+                            if (fieldId == != 'confname' && fieldId != 'type' && fieldId != 'auth') {
+                                document.getElementsByName(fieldId)[0].parentNode.parentNode.classList.add('hidden');
+                            }
                         }
                     });
                 });
