f313982e5a807e05e60ba7dc74fed076cd159ef6
donicid4p <marius.donici@data4prime.com>
2024-09-13 17:28:41 +0200

    Testing Licensing

--- app/Services/ConnectorService.php
+++ app/Services/ConnectorService.php
@@ -45,6 +45,7 @@ class ConnectorService
                 'Content-Type' => 'application/json',
                 'Accept' => 'application/json',
             ])->post($url, $data);
+            dd($response);
 
             if ($response->ok()) {
                 $license = $response->json();
