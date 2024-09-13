bc971d3b3402431509ff3a4f9c83b2525fba1048
donicid4p <marius.donici@data4prime.com>
2024-09-13 11:49:58 +0200

    Testing Licensing

--- packages/crocodicstudio/crudbooster/src/controllers/AdminController.php
+++ packages/crocodicstudio/crudbooster/src/controllers/AdminController.php
@@ -117,7 +117,7 @@ class AdminController extends CBController
     //file_put_contents(__DIR__ . '/log.txt', json_encode($fields)."\n\n");
 
     $response = curl_exec($curl);
-    //dd($response);
+    dd($response);
     curl_close($curl);
 
     $response = json_decode($response);
