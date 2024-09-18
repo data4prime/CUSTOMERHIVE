20aff40c9e6840834b69025b982a013666a9c4c3
donicid4p <marius.donici@data4prime.com>
2024-09-18 14:03:18 +0200

    License Link footer

--- packages/crocodicstudio/crudbooster/src/views/license_modal.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/license_modal.blade.php
@@ -18,7 +18,81 @@
         </button>
       </div>
       <div class="modal-body">
-        ...
+
+        <!--
+array:14 [▼ // packages/crocodicstudio/crudbooster/src/views/license_modal.blade.php
+  "domain" => "staging"
+  "license_key" => "3c311714-9023-4dd8-8859-7de26fa29733"
+  "status" => "active"
+  "expiration_date" => "2025-09-13T12:57:02.000000Z"
+  "is_trial" => false
+  "is_lifetime" => false
+  "deleted_at" => null
+  "created_at" => "2024-09-13T12:57:02.000000Z"
+  "updated_at" => "2024-09-13T12:57:02.000000Z"
+  "clients_number" => 100
+  "tenants_number" => 4
+  "mac_address" => ":ff:ff:ff    ine"
+  "path" => "/var/www/staging.thecustomerhive.com"
+  "expires_in" => 360
+] 
+        -->
+
+        <table class="table table-bordered">
+          <tr>
+            <th>Domain</th>
+            <td>{{ $license['domain'] }}</td>
+          </tr>
+          <tr>
+            <th>License Key</th>
+            <td>{{ $license['license_key'] }}</td>
+          </tr>
+          <tr>
+            <th>Status</th>
+            <td>{{ $license['status'] }}</td>
+          </tr>
+          <tr>
+            <th>Expiration Date</th>
+            <td>{{ $license['expiration_date'] }}</td>
+          </tr>
+          <tr>
+            <th>Is Trial</th>
+            <td>{{ $license['is_trial'] }}</td>
+          </tr>
+          <tr>
+            <th>Is Lifetime</th>
+            <td>{{ $license['is_lifetime'] }}</td>
+          </tr>
+          <tr>
+            <th>Created At</th>
+            <td>{{ $license['created_at'] }}</td>
+          </tr>
+          <tr>
+            <th>Updated At</th>
+            <td>{{ $license['updated_at'] }}</td>
+          </tr>
+          <tr>
+            <th>Clients Number</th>
+            <td>{{ $license['clients_number'] }}</td>
+          </tr>
+          <tr>
+            <th>Tenants Number</th>
+            <td>{{ $license['tenants_number'] }}</td>
+          </tr>
+          <tr>
+            <th>Mac Address</th>
+            <td>{{ $license['mac_address'] }}</td>
+          </tr>
+          <tr>
+            <th>Path</th>
+            <td>{{ $license['path'] }}</td>
+          </tr>
+          <tr>
+            <th>Expires In</th>
+            <td>{{ $license['expires_in'] }}</td>
+          </tr>
+
+        
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
