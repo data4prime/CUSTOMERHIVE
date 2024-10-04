328ed648c9211721513e6e4734452b6d903afb80
donicid4p <marius.donici@data4prime.com>
2024-10-04 12:52:53 +0200

    Module Helpers

--- database/migrations/2024_10_04_123959_create_table_module_helpers_table.php
+++ database/migrations/2024_10_04_123959_create_table_module_helpers_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('table_module_helpers', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('url');
+
+
             $table->timestamps();
         });
     }
