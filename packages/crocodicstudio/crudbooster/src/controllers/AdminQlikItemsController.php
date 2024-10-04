297c7deb6647c12725353ff3729fce136fb900ee
donicid4p <marius.donici@data4prime.com>
2024-10-04 16:06:21 +0200

    Module Helpers

--- packages/crocodicstudio/crudbooster/src/controllers/AdminQlikItemsController.php
+++ packages/crocodicstudio/crudbooster/src/controllers/AdminQlikItemsController.php
@@ -44,7 +44,7 @@ class AdminQlikItemsController extends CBController
 		$this->col = [];
 		$this->col[] = ["label" => "Title", "name" => "title"];
 		$this->col[] = ["label" => "Subtitle", "name" => "subtitle"];
-		$this->col[] = ["label" => "Help", "name" => "description"];
+		//$this->col[] = ["label" => "Help", "name" => "description"];
 		$this->col[] = ["label" => "Public", "name" => "proxy_token"];
 		//$this->col[] = ["label" => "Qlik Conf", "name" => "qlik_conf"];
 		$this->col[] = array("label" => "Qlik Conf", "name" => "qlik_conf", "join" => "qlik_confs,confname");
@@ -56,7 +56,7 @@ class AdminQlikItemsController extends CBController
 		$this->form[] = ['label' => 'Title', 'name' => 'title', 'type' => 'text', 'validation' => 'required|string|min:1|max:70', 'width' => 'col-sm-10', 'placeholder' => 'Item title'];
 		$this->form[] = ['label' => 'Url', 'name' => 'url', 'type' => 'text', 'validation' => 'required|min:1|max:255', 'width' => 'col-sm-10', 'placeholder' => 'Path to embed item'];
 		$this->form[] = ['label' => 'Subtitle', 'name' => 'subtitle', 'type' => 'text', 'validation' => 'string|min:1|max:70', 'width' => 'col-sm-10', 'placeholder' => 'Item subtitle'];
-		$this->form[] = ['label' => 'Help', 'name' => 'description', 'type' => 'textarea', 'validation' => 'string|min:1|max:200', 'width' => 'col-sm-10', 'placeholder' => 'Item description'];
+		$this->form[] = ['label' => 'URL Help', 'name' => 'url_help', 'type' => 'textarea', 'validation' => 'string|min:1|max:200', 'width' => 'col-sm-10', 'placeholder' => 'Item helper'];
 		$this->form[] = ['label' => 'Enable public access', 'name' => 'public_access', 'type' => 'checkbox', 'width' => 'col-sm-1'];
 		$this->form[] = ['label' => 'Qlik Configuration', 'name' => 'qlik_conf', "type" => "select", "datatable" => "qlik_confs,confname", 'width' => 'col-sm-10'];
 		# END FORM DO NOT REMOVE THIS LINE
