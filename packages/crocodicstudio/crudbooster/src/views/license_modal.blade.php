e6f7b4077926e74d000e2c1874aad7c71edf6684
donicid4p <marius.donici@data4prime.com>
2024-10-11 11:05:58 +0200

    Trial in Autonomia

--- packages/crocodicstudio/crudbooster/src/views/license_modal.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/license_modal.blade.php
@@ -26,11 +26,11 @@
 @endphp
 
 
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="licenseModal" tabindex="-1" role="dialog" aria-labelledby="licenseModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">{{ trans('crudbooster.license') }}</h5>
+        <h5 class="modal-title" id="licenseModalLabel">{{ trans('crudbooster.license') }}</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
