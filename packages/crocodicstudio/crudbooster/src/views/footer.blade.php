6a80c433e137f389eb6ca4c8415963a681f55634
donicid4p <marius.donici@data4prime.com>
2024-10-11 12:46:05 +0200

    Trial in Autonomia

--- packages/crocodicstudio/crudbooster/src/views/footer.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/footer.blade.php
@@ -5,23 +5,6 @@
         &nbsp;|&nbsp;<a  data-toggle="modal" data-target="#licenseModal">{{ trans('crudbooster.license') }}</a>
     </div>-->
 
-    <div class="pull-{{ trans('crudbooster.right') }} hidden-xs">
-
-        &nbsp;|&nbsp;<a data-bs-toggle="modal"   data-bs-target="#licenseModal">{{ trans('crudbooster.license') }} BS</a>
-    </div>
-
-
-    <div class="pull-{{ trans('crudbooster.right') }} hidden-xs">
-        {{ trans('crudbooster.powered_by') }} Data4Prime
-    </div>
-
-    <div style="margin-right:15px;" class="pull-{{ trans('crudbooster.right') }} hidden-xs">
-        {{Session::get('appname')}} {{ config('app.version') }}
-    </div>
-    <!-- Default to the left -->
-    <strong>{{ trans('crudbooster.copyright') }} &copy; <?php echo date('Y') ?>. {{ trans('crudbooster.all_rights_reserved') }} .</strong>
-</footer>
-
 <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
   Launch demo modal
@@ -47,6 +30,25 @@
     </div>
   </div>
 </div>
+
+    <div class="pull-{{ trans('crudbooster.right') }} hidden-xs">
+
+        &nbsp;|&nbsp;<a data-bs-toggle="modal"   data-bs-target="#licenseModal">{{ trans('crudbooster.license') }} BS</a>
+    </div>
+
+
+    <div class="pull-{{ trans('crudbooster.right') }} hidden-xs">
+        {{ trans('crudbooster.powered_by') }} Data4Prime
+    </div>
+
+    <div style="margin-right:15px;" class="pull-{{ trans('crudbooster.right') }} hidden-xs">
+        {{Session::get('appname')}} {{ config('app.version') }}
+    </div>
+    <!-- Default to the left -->
+    <strong>{{ trans('crudbooster.copyright') }} &copy; <?php echo date('Y') ?>. {{ trans('crudbooster.all_rights_reserved') }} .</strong>
+</footer>
+
+
 
 
 
