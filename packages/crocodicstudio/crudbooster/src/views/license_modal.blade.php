d0c9e7e73c8332759358ebea1aa25242e8673b42
donicid4p <marius.donici@data4prime.com>
2024-10-14 15:42:46 +0200

    Update Bootstrap

--- packages/crocodicstudio/crudbooster/src/views/license_modal.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/license_modal.blade.php
@@ -29,9 +29,9 @@
 <div class="modal fade" id="licenseModal" tabindex="-1" role="dialog" aria-labelledby="licenseModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <div class="modal-header">
+      <div class="modal-header" style="justify-content: space-between;">
         <h5 class="modal-title" id="licenseModalLabel">{{ trans('crudbooster.license') }}</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
