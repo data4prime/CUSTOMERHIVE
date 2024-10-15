fa10908addc36c74e092584b64b11e31eb2dde99
donicid4p <marius.donici@data4prime.com>
2024-10-15 12:37:34 +0200

    Bootstrap update

--- packages/crocodicstudio/crudbooster/src/views/statistic_builder/index.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/statistic_builder/index.blade.php
@@ -361,7 +361,7 @@
 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                         aria-hidden="true">&times;</span></button>
             </div>
-            <div class="modal-body">
+            <div class="modal-body" style="padding: 30px;">
                 <p>One fine body&hellip;</p>
             </div>
             <div class="modal-footer">
