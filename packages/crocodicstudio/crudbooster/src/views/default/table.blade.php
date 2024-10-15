f2039810199fadc27f27efd86feadbaf2148d77e
donicid4p <marius.donici@data4prime.com>
2024-10-15 09:55:20 +0200

    Bootstrap update

--- packages/crocodicstudio/crudbooster/src/views/default/table.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/default/table.blade.php
@@ -184,7 +184,7 @@
 
 </form><!--END FORM TABLE-->
 
-<div class="col-md-8">{!! urldecode(str_replace("/?","?",$result->appends(Request::all())->render())) !!}</div>
+<div class="col-md-8">{!! urldecode(str_replace("/?","?",$result->appends(Request::all())->links() )) !!}</div>
 <?php
 $from = $result->count() ? ($result->perPage() * $result->currentPage() - $result->perPage() + 1) : 0;
 $to = $result->perPage() * $result->currentPage() - $result->perPage() + $result->count();
