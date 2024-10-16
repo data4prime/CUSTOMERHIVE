87c903522c6b6df774822affc03f5be8b5b6266b
donicid4p <marius.donici@data4prime.com>
2024-10-16 14:21:53 +0200

    Bootstrap update

--- packages/crocodicstudio/crudbooster/src/views/module_generator/navigation.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/module_generator/navigation.blade.php
@@ -5,15 +5,15 @@
       Step 1 - Module Information</a>
     </li>
     <li role=" presentation" class="nav-item @if($active_tab == 2)active @endif">
-      <a class="nav-link @if($active_tab == 1) active @endif " href="@if(isset($id)){{Route('ModulsControllerGetStep2').'/'.$id}} @else#@endif"><i class='fa fa-table'></i> Step 2 - Data structure</a>
+      <a class="nav-link @if($active_tab == 2) active @endif " href="@if(isset($id)){{Route('ModulsControllerGetStep2').'/'.$id}} @else#@endif"><i class='fa fa-table'></i> Step 2 - Data structure</a>
     </li>
     <li role=" presentation" class="nav-item @if($active_tab == 3)active @endif">
-      <a class="nav-link @if($active_tab == 1) active @endif " href="@if(isset($id)){{Route('ModulsControllerGetStep3').'/'.$id}} @else#@endif"><i class='fa fa-table'></i> Step 3 - List Settings</a>
+      <a class="nav-link @if($active_tab == 2) active @endif " href="@if(isset($id)){{Route('ModulsControllerGetStep3').'/'.$id}} @else#@endif"><i class='fa fa-table'></i> Step 3 - List Settings</a>
     </li>
     <li role=" presentation" class="nav-item @if($active_tab == 4)active @endif">
-      <a class="nav-link @if($active_tab == 1) active @endif " href="@if(isset($id)){{Route('ModulsControllerGetStep4').'/'.$id}} @else#@endif"><i class='fa fa-plus-square-o'></i> Step 4 - Form Settings</a>
+      <a class="nav-link @if($active_tab == 4) active @endif " href="@if(isset($id)){{Route('ModulsControllerGetStep4').'/'.$id}} @else#@endif"><i class='fa fa-plus-square-o'></i> Step 4 - Form Settings</a>
     </li>
     <li role=" presentation" class="nav-item @if($active_tab == 5)active @endif">
-      <a class="nav-link @if($active_tab == 1) active @endif " href="@if(isset($id)){{Route('ModulsControllerGetStep5').'/'.$id}} @else#@endif"><i class='fa fa-wrench'></i> Step 5 - Configuration</a>
+      <a class="nav-link @if($active_tab == 5) active @endif " href="@if(isset($id)){{Route('ModulsControllerGetStep5').'/'.$id}} @else#@endif"><i class='fa fa-wrench'></i> Step 5 - Configuration</a>
     </li>
 </ul>
