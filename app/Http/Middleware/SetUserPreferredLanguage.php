07ee17dff91a744137fa974816d7b86bf4375892
donicid4p <marius.donici@data4prime.com>
2024-10-17 10:45:00 +0200

    User Lang

--- app/Http/Middleware/SetUserPreferredLanguage.php
+++ app/Http/Middleware/SetUserPreferredLanguage.php
@@ -5,6 +5,9 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
+
 class SetUserPreferredLanguage
 {
     /**
@@ -16,6 +19,10 @@ class SetUserPreferredLanguage
      */
     public function handle(Request $request, Closure $next)
     {
+
+        if (Auth::check() && Auth::user()->lang) {
+            App::setLocale(Auth::user()->lang);
+        }
         return $next($request);
     }
 }
