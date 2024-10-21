06d5a788582cdfb6447e1e564facf0f2e9c70396
donicid4p <marius.donici@data4prime.com>
2024-10-21 11:51:41 +0200

    Module Generator

--- packages/crocodicstudio/crudbooster/src/views/module_generator/step1.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/module_generator/step1.blade.php
@@ -20,15 +20,15 @@
 </script>
 @endpush
 
-<div class="box box-default">
-    <div class="box-header with-border">
+<div class="card box-default">
+    <div class="card-header mb-3 with-border">
         <h3 class="box-title">Module Information</h3>
     </div>
-    <div class="box-body">
+    <div class="card-body">
         <form method="post" action="{{Route('ModulsControllerPostStep1')}}">
             <input type="hidden" name="_token" value="{{csrf_token()}}">
             <input type="hidden" name="id" value="{{isset($row->id) ? $row->id : ''}}">
-            <div class="form-group">
+            <div class="mb-3 row">
                 <label for="">Table</label>
                 <select name="table" id="table" required class="select2 form-control"
                     value="{{ isset($row->table_name) ? $row->table_name : ''}}">
@@ -44,9 +44,9 @@
                     Create a new table or use an existing one
                 </div>
             </div>
-            <div class="form-group">
+            <div class="mb-3 row">
                 <label for="">Module Name</label>
-                <input type="text" class="form-control" required name="name"
+                <input style="margin-left: 12px;" type="text" class="form-control" required name="name"
                     value="{{ isset($row->name) ? $row->name : ''}}">
                 <div class="help-block">
                     Symbols, special characters and accents are forbidden. Use only alphanumeric lower case characters
@@ -54,7 +54,7 @@
                 </div>
             </div>
 
-            <div class="form-group">
+            <div class="mb-3 row">
                 <label for="">Icon</label>
                 <select name="icon" id="icon" required class="select2 form-control">
                     @foreach($fontawesome as $f)
@@ -64,13 +64,50 @@
                 </select>
             </div>
 
-            <!-- <div class="form-group">
+
+            <div class="row mb-3">
+                <label for="table" class="col-sm-2 col-form-label col-form-label-sm">Table</label>
+                <div class="col-sm-10">
+                    <select name="table" id="table" required class="select2 form-control"
+                        value="{{ isset($row->table_name) ? $row->table_name : ''}}">
+                        <option value="new">** Create a new table</option>
+                        @foreach($tables_list as $table)
+
+                        <option {{(isset($row->table_name) && $table==$row->table_name)?"selected":""}}
+                            value="{{$table}}">{{$table}}</option>
+
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label for="name" class="col-sm-2 col-form-label">Module Name</label>
+                <div class="col-sm-10">
+                    <input style="margin-left: 12px;" type="text" class="form-control" required name="name"
+                    value="{{ isset($row->name) ? $row->name : ''}}">
+                </div>
+            </div>
+
+            <div class="row">
+                <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Icon</label>
+                <div class="col-sm-10">
+                <select name="icon" id="icon" required class="select2 form-control">
+                    @foreach($fontawesome as $f)
+                    <option {{(isset($row->icon) && $row->icon == 'fa fa-'.$f)?"selected":""}} value="fa
+                        fa-{{$f}}">{{$f}}</option>
+                    @endforeach
+                </select>
+                </div>
+            </div>
+
+            <!-- <div class="mb-3 row">
                     <label for="">Module Slug</label>
                     <input type="text" class="form-control" required name="path" value="{{isset($row->path) ? $row->path : '' }}">
                     <div class="help-block">Alphanumeric and underscore characters only</div>
                 </div> -->
     </div>
-    <div class="box-footer">
+    <div class="card-footer">
 
         <input checked type='checkbox' name='create_menu' value='1' /> Also create menu for this module <a href='#'
             title='If you check this, we will create the menu for this module'>(?)</a>
