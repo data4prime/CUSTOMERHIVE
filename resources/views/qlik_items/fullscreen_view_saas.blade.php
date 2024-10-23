68c8cee7248c90a34bc622a74f6f4d9c2049942c
donicid4p <marius.donici@data4prime.com>
2024-10-23 15:17:41 +0200

    bug fixing

--- resources/views/qlik_items/fullscreen_view_saas.blade.php
+++ resources/views/qlik_items/fullscreen_view_saas.blade.php
@@ -6,10 +6,10 @@ $debug_url = $item_url;
 
 
 @endphp
-@push('head')
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-@endpush
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
 <div class="card">
   <div class="card-header">
     <a href="{{$debug_url}}" target="_blank">{{$debug_url}}</a>
