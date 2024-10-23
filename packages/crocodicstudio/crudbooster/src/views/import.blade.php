4784efab0877419a8c7ab5c214e9047f260f624e
donicid4p <marius.donici@data4prime.com>
2024-10-23 10:54:19 +0200

    bug fixing

--- packages/crocodicstudio/crudbooster/src/views/import.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/import.blade.php
@@ -15,8 +15,9 @@
 
         <ul class='nav nav-tabs'>
             <li class="nav-item" style="background:#eeeeee">
-                <a class="nav-link" style="color:#111" onclick="if(confirm('Are you sure want to leave ?')) location.href='{{ CRUDBooster::mainpath("import-data") }}'"
-                                              href='javascript:;'><i class='fa fa-download'></i> Upload a File &raquo;
+                <a class="nav-link" style="color:#111" 
+                    onclick="if(confirm('Are you sure want to leave ?')) location.href='{{ CRUDBooster::mainpath("import-data") }}'"
+                    href='javascript:;'><i class='fa fa-download'></i> Upload a File &raquo;
                 </a>
             </li>
             <li class="nav-item" style="background:#eeeeee">
@@ -97,11 +98,11 @@
     @if(Request::get('file') && !Request::get('import'))
 
         <ul class='nav nav-tabs'>
-            <li style="background:#eeeeee"><a style="color:#111"
+            <li  class="nav-item" style="background:#eeeeee"><a style="color:#111"
                                               onclick="if(confirm('Are you sure want to leave ?')) location.href='{{ CRUDBooster::mainpath("import-data") }}'"
                                               href='javascript:;'><i class='fa fa-download'></i> Upload a File &raquo;</a></li>
-            <li style="background:#ffffff" class='active'><a style="color:#111" href='#'><i class='fa fa-cogs'></i> Adjustment &raquo;</a></li>
-            <li style="background:#eeeeee"><a style="color:#111" href='#'><i class='fa fa-cloud-download'></i> Importing &raquo;</a></li>
+            <li  class="nav-item" style="background:#ffffff" class='active'><a style="color:#111" href='#'><i class='fa fa-cogs'></i> Adjustment &raquo;</a></li>
+            <li  class="nav-item" style="background:#eeeeee"><a style="color:#111" href='#'><i class='fa fa-cloud-download'></i> Importing &raquo;</a></li>
         </ul>
 
         <!-- Box -->
