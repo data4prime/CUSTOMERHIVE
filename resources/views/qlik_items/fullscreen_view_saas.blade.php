b613ae8fdd347825d521b40be05adabac891488b
donicid4p <marius.donici@data4prime.com>
2024-10-23 16:11:11 +0200

    bug fixing

--- resources/views/qlik_items/fullscreen_view_saas.blade.php
+++ resources/views/qlik_items/fullscreen_view_saas.blade.php
@@ -9,7 +9,7 @@ $debug_url = $item_url;
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-@include('crudbooster::header')
+
 <div class="card">
   <div class="card-header">
     <h4 class="qi_subtitle">{{ $page_title }}</h4>
