2a756f5224d6956bfa216dd6c0a125da3b59ad76
donicid4p <marius.donici@data4prime.com>
2024-10-23 15:16:03 +0200

    bug fixing

--- resources/views/qlik_items/fullscreen_view_saas.blade.php
+++ resources/views/qlik_items/fullscreen_view_saas.blade.php
@@ -62,3 +62,7 @@ $debug_url = $item_url;
 </script>
 <script src="@php echo asset($js_login) @endphp"></script>
 
+@push('head')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+@endpush
