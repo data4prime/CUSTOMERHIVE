280f92986d32aaf1daf074001571f4bba2a40999
donicid4p <marius.donici@data4prime.com>
2024-10-29 17:34:44 +0100

    Small Box

--- packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/qlikwidget.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/qlikwidget.blade.php
@@ -139,3 +139,16 @@ function update_objects(select){
 
     ?>
 @endif
+
+<script defer>
+if (!window.location.href.includes('statistic_builder/builder')) {
+    //jquery get div with action class
+    var action = $('#{{$componentID}}').find('.action');
+    //make it disappear
+    action.hide();
+
+
+    
+
+}
+</script>
