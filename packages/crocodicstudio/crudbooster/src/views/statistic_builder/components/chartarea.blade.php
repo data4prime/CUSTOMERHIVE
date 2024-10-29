07b25dc9269eba6545dd7f692210c086b60f573f
donicid4p <marius.donici@data4prime.com>
2024-10-29 16:57:31 +0100

    Small Box

--- packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/chartarea.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/statistic_builder/components/chartarea.blade.php
@@ -74,8 +74,8 @@
                 $raw = DB::raw($sql)->getValue(DB::getQueryGrammar());
                 echo $raw;
                 //replace ' with " to avoid error
-                //$raw = str_replace("'", '"', $raw);
-                //$query = DB::select($raw);
+                $raw = str_replace("'", '', $raw);
+                $query = DB::select($raw);
                 foreach ($query as $r) {
                     $datax[] = isset($r->label) ? $r->label : '';
                     $datamerger[] = isset($r->value) ? $r->value : '';
