a34cdc9395afe649222949ade45f2a37a7cacf34
donicid4p <marius.donici@data4prime.com>
2024-10-29 14:14:39 +0100

    Small Box

--- packages/crocodicstudio/crudbooster/src/views/statistic_builder/index.blade.php
+++ packages/crocodicstudio/crudbooster/src/views/statistic_builder/index.blade.php
@@ -1,195 +1,70 @@
-@push('bottom')
+@push('head')
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
-<!--<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>-->
-<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
-
-
-<script type="text/javascript">
-    $(function () {
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-        $(document).ajaxStart(function () {
-            $('.btn-save-statistic').html("<i class='fa fa-spin fa-spinner'></i>");
-        })
-        $(document).ajaxStop(function () {
-            $('.btn-save-statistic').html("<i class='fa fa-save'></i> Auto Save Ready");
-        })
-
-        $('.btn-show-sidebar').click(function (e) {
-            e.stopPropagation();
-        })
-        $('html,body').click(function () {
-            $('.control-sidebar').removeClass('control-sidebar-open');
-        })
-    })
-</script>
-@endpush
-@push('head')
-<style type="text/css">
-.statistic-row>div {
-
-/*overflow: auto;*/
-
-}
-    .control-sidebar ul {
-        padding: 0 0 0 0;
-        margin: 0 0 0 0;
-        list-style-type: none;
-    }
-
-    .control-sidebar ul li {
-        text-align: center;
-        padding: 10px;
-        border-bottom: 1px solid #555555;
-    }
-
-    .control-sidebar ul li:hover {
-        background: #555555;
-    }
-
-    .control-sidebar ul li .title {
-        text-align: center;
-        color: #ffffff;
-    }
-
-    .control-sidebar ul li img {
-        width: 100%;
-    }
-
-    ::-webkit-scrollbar {
-        width: 5px;
-        height: 5px;
-    }
-</style>
-@endpush
-
-@push('bottom')
-<!-- ADDITION FUNCTION FOR BUTTON -->
-<script type="text/javascript">
-    var id_cms_statistics = '{{$id_cms_statistics}}';
-
-    function addWidget(id_cms_statistics, area, component) {
-        console.log("Add Widget " + component + " to " + area);
-        var id = new Date().getTime();
-        $('#' + area).append("<div id='" + id + "' class='area-loading'><i class='fa fa-spin fa-spinner'></i></div>");
-
-        var sorting = $('#' + area + ' .border-box').length;
-        $.post("{{CRUDBooster::mainpath('add-component')}}", {
-            component_name: component,
-            id_cms_statistics: id_cms_statistics,
-            sorting: sorting,
-            area: area
-        }, function (response) {
-            $('#' + area).append(response.layout);
-            $('#' + id).remove();
-        })
-    }
-
-</script>
-<!--DATATABLE-->
-<link rel="stylesheet"
-    href="{{ asset ('vendor/crudbooster/assets/adminlte/plugins/datatables/dataTables.bootstrap.css')}}">
-<script src="{{ asset ('vendor/crudbooster/assets/adminlte/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-<script src="{{ asset ('vendor/crudbooster/assets/adminlte/plugins/datatables/dataTables.bootstrap.min.js')}}"></script>
-<!--END HERE-->
-@endpush
-
-
-@push('head')
-<!-- jQuery UI 1.11.4 -->
-<style>
-    .sort-highlight {
-        border: 3px dashed #cccccc;
-    }
-
-    .layout-grid {
-        border: 1px dashed #cccccc;
-        min-height: 150px;
-    }
-
-    .layout-grid + .layout-grid {
-        border-left: 1px dashed transparent;
-    }
-
-    .border-box {
-        position: relative;
-    }
-
-    .border-box .action {
-        font-size: 20px;
-        display: none;
-        text-align: center;
-        padding: 3px 5px;
-        background: #DD4B39;
-        color: #ffffff;
-        width: 70px;
-        border-radius: 5px 5px 0 0;
-        position: absolute;
-        margin-top: -20px;
-        right: 0;
-        z-index: 999;
-        opacity: 0.8;
-    }
-
-    .border-box .action a {
-        color: #ffffff;
-    }
-
-    @if (CRUDBooster::getCurrentMethod() == 'getBuilder')
-        .border-box:hover .action {
-            display: block;
-        }
-    @endif
-
-    .card {
-        margin-bottom: 0;
-    }
-
-    .card-header,
-    .inner-box,
-    .box-header.mb-3,
-    .btn-add-widget {
-        cursor: move;
-    }
-
-    .connectedSortable {
-        position: relative;
-    }
-
-    .area-loading {
-        position: relative;
-        width: 100%;
-        height: 130px;
-        background: #dedede;
-        border: 4px dashed #cccccc;
-        font-size: 50px;
-        color: #aaaaaa;
-        margin-bottom: 20px;
-    }
-
-    .area-loading i {
-        position: absolute;
-        left: 50%;
-        top: 30%;
-        transform: translate(-50%, -50%);
-    }
-</style>
+<link rel="stylesheet" href="{{ asset('vendor/crudbooster/assets/adminlte/plugins/datatables/dataTables.bootstrap.css') }}">
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<style>
+    .statistic-row > div { /*overflow: auto;*/ }
+    .control-sidebar ul { padding: 0; margin: 0; list-style-type: none; }
+    .control-sidebar ul li { text-align: center; padding: 10px; border-bottom: 1px solid #555; }
+    .control-sidebar ul li:hover { background: #555; }
+    .control-sidebar ul li .title { color: #fff; }
+    .control-sidebar ul li img { width: 100%; }
+    ::-webkit-scrollbar { width: 5px; height: 5px; }
+
+    .sort-highlight { border: 3px dashed #cccccc; }
+    .layout-grid { border: 1px dashed #cccccc; min-height: 150px; }
+    .border-box { position: relative; }
+    .border-box .action { /* existing styles */ }
+    .connectedSortable { position: relative; }
+    .area-loading { /* existing styles */ }
+    .area-loading i { /* existing styles */ }
+</style>
 @endpush
 
 @push('bottom')
+<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+<script src="{{ asset('vendor/crudbooster/assets/adminlte/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('vendor/crudbooster/assets/adminlte/plugins/datatables/dataTables.bootstrap.min.js') }}"></script>
 <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+
 <script type="text/javascript">
     $(function () {
-        var cloneSidebar = $('.control-sidebar').clone();
+        // AJAX Setup
+        $.ajaxSetup({
+            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }
+        });
 
+        // Spinner Handling
+        $(document).on({
+            ajaxStart: () => $('.btn-save-statistic').html("<i class='fa fa-spin fa-spinner'></i>"),
+            ajaxStop: () => $('.btn-save-statistic').html("<i class='fa fa-save'></i> Auto Save Ready")
+        });
+
+        // Control Sidebar
+        $('.btn-show-sidebar').click(e => e.stopPropagation());
+        $('html, body').click(() => $('.control-sidebar').removeClass('control-sidebar-open'));
+
+        // Add Widget Functionality
+        const id_cms_statistics = '{{$id_cms_statistics}}';
+        function addWidget(area, component) {
+            const id = Date.now();
+            $('#' + area).append(`<div id="${id}" class="area-loading"><i class="fa fa-spin fa-spinner"></i></div>`);
+            const sorting = $('#' + area + ' .border-box').length;
+
+            $.post("{{ CRUDBooster::mainpath('add-component') }}", {
+                component_name: component,
+                id_cms_statistics: id_cms_statistics,
+                sorting: sorting,
+                area: area
+            }, response => {
+                $('#' + area).append(response.layout);
+                $('#' + id).remove();
+            });
+        }
+
+        // Sorting
         @if (CRUDBooster::getCurrentMethod() == 'getBuilder')
-            createSortable();
-        @endif
-
         function createSortable() {
             $(".connectedSortable").sortable({
                 placeholder: "sort-highlight",
@@ -198,25 +73,17 @@
                 forcePlaceholderSize: true,
                 zIndex: 999999,
                 stop: function (event, ui) {
-                    var className = ui.item.attr('class');
-                    var idName = ui.item.attr('id');
-
-                    if (className === 'button-widget-area') {
-                        var areaName = $('#' + idName).parent('.connectedSortable').attr('id');
-                        var component = $('#' + idName + ' > a').data('component');
-                        $('#' + idName).remove();
-                        addWidget(id_cms_statistics, areaName, component);
-                        $('.control-sidebar').html(cloneSidebar);
-                        cloneSidebar = $('.control-sidebar').clone();
-                        createSortable();
-                    }
+                    const idName = ui.item.attr('id');
+                    const areaName = $('#' + idName).parent('.connectedSortable').attr('id');
+                    const component = $('#' + idName + ' > a').data('component');
+                    $('#' + idName).remove();
+                    addWidget(areaName, component);
                 },
                 update: function (event, ui) {
                     if (ui.sender) {
-                        var componentID = ui.item.attr('id');
-                        var areaName = $('#' + componentID).parent('.connectedSortable').attr("id");
-                        var index = $('#' + componentID).index();
-
+                        const componentID = ui.item.attr('id');
+                        const areaName = $('#' + componentID).parent('.connectedSortable').attr("id");
+                        const index = $('#' + componentID).index();
                         $.post("{{ CRUDBooster::mainpath('update-area-component') }}", {
                             componentid: componentID,
                             sorting: index,
@@ -226,16 +93,17 @@
                 }
             });
         }
+        createSortable();
+        @endif
 
+        // Load Components
         $('.connectedSortable').each(function () {
-            var areaName = $(this).attr('id');
-
+            const areaName = $(this).attr('id');
             $.get("{{ CRUDBooster::adminpath('statistic_builder/list-component') }}/" + id_cms_statistics + "/" + areaName, function (response) {
                 if (response.components) {
-                    $.each(response.components, function (i, obj) {
-                        var loadingDiv = $("<div id='area-loading-" + obj.componentID + "' class='area-loading'><i class='fa fa-spin fa-spinner'></i></div>");
+                    response.components.forEach(obj => {
+                        const loadingDiv = $(`<div id='area-loading-${obj.componentID}' class='area-loading'><i class='fa fa-spin fa-spinner'></i></div>`);
                         $('#' + areaName).append(loadingDiv);
-                        
                         $.get("{{ CRUDBooster::adminpath('statistic_builder/view-component') }}/" + obj.componentID, function (view) {
                             loadingDiv.remove();
                             $('#' + areaName).append(view.layout);
@@ -245,62 +113,52 @@
             });
         });
 
+        // Delete Component
         $(document).on('click', '.btn-delete-component', function () {
-            var componentID = $(this).data('componentid');
-            var $this = $(this);
-
+            const componentID = $(this).data('componentid');
+            const $this = $(this);
             swal({
                 title: "Are you sure?",
                 text: "You will not be able to recover this widget!",
-                type: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#DD6B55",
-                confirmButtonText: "Yes",
-                closeOnConfirm: true
-            }, function () {
-                $.get("{{ CRUDBooster::mainpath('delete-component') }}/" + componentID, function () {
-                    $this.parents('.border-box').remove();
-                });
+                icon: "warning",
+                buttons: true,
+            }).then(willDelete => {
+                if (willDelete) {
+                    $.get("{{ CRUDBooster::mainpath('delete-component') }}/" + componentID, () => {
+                        $this.parents('.border-box').remove();
+                    });
+                }
             });
         });
 
+        // Edit Component
         $(document).on('click', '.btn-edit-component', function () {
-            var componentID = $(this).data('componentid');
-            var name = $(this).data('name');
+            const componentID = $(this).data('componentid');
+            const name = $(this).data('name');
 
             $('#modal-statistic .modal-title').text(name);
             $('#modal-statistic .modal-body').html("<i class='fa fa-spin fa-spinner'></i> Please wait loading...");
             $('#modal-statistic').modal('show');
 
-            $.get("{{ CRUDBooster::mainpath('edit-component') }}/" + componentID, function (response) {
+            $.get("{{ CRUDBooster::mainpath('edit-component') }}/" + componentID, response => {
                 $('#modal-statistic .modal-body').html(response);
             });
         });
 
+        // Modal Submit
         $('#modal-statistic .btn-submit').click(function () {
-            $('#modal-statistic form .has-error').removeClass('has-error');
-
-            var requiredInputs = [];
-            $('#modal-statistic form').find('input[required], textarea[required], select[required]').each(function () {
-                var $input = $(this);
-                if (!$input.val()) {
-                    requiredInputs.push($input.attr('name'));
-                }
-            });
+            const $form = $('#modal-statistic form');
+            $form.find('.has-error').removeClass('has-error');
+            const requiredInputs = $form.find('input[required], textarea[required], select[required]').filter((_, input) => !$(input).val()).map((_, input) => $(input).attr('name')).toArray();
 
             if (requiredInputs.length) {
-                setTimeout(function () {
-                    $.each(requiredInputs, function (i, name) {
-                        $('#modal-statistic form').find('[name="' + name + '"]').parent('.mb-3.row').addClass('has-error');
-                    });
-                }, 200);
+                setTimeout(() => requiredInputs.forEach(name => $form.find(`[name="${name}"]`).parent('.mb-3.row').addClass('has-error')), 200);
                 return false;
             }
 
-            var $button = $(this).text('Saving...').addClass('disabled');
-
+            const $button = $(this).text('Saving...').addClass('disabled');
             $.ajax({
-                data: $('#modal-statistic form').serialize(),
+                data: $form.serialize(),
                 type: 'POST',
                 url: "{{ CRUDBooster::mainpath('save-component') }}",
                 success: function () {
@@ -330,71 +188,31 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary btn-submit" data-bs-loading-text="Saving..." autocomplete="off">
-                    Save Changes
-                </button>
+                <button type="button" class="btn btn-primary btn-submit" data-bs-loading-text="Saving..." autocomplete="off">Save Changes</button>
             </div>
         </div>
     </div>
 </div>
 
-
-<div id='statistic-area'>
-
-@if (!empty($code_layout)) 
-    @php echo  str_replace("", "", $code_layout) @endphp
-@else 
-
-<div class="statistic-row row">
-        <div id='area1' class="col-sm-3 connectedSortable">
-
+<div id="statistic-area">
+    @if (!empty($code_layout))
+        {!! str_replace("", "", $code_layout) !!}
+    @else
+        <div class="statistic-row row">
+            @for ($i = 1; $i <= 8; $i++)
+                <div id="area{{ $i }}" class="col-sm-3 connectedSortable"></div>
+            @endfor
         </div>
-        <div id='area2' class="col-sm-3 connectedSortable">
-
+        <div class='statistic-row row'>
+            <div id='area9' class="col-sm-12 connectedSortable"></div>
         </div>
-        <div id='area3' class="col-sm-3 connectedSortable">
-
-        </div>
-        <div id='area4' class="col-sm-3 connectedSortable">
-
-        </div>
-    </div>
-
-<div class="statistic-row row">
-        <div id='area5' class="col-sm-3 connectedSortable">
-
-        </div>
-        <div id='area6' class="col-sm-3 connectedSortable">
-
-        </div>
-        <div id='area7' class="col-sm-3 connectedSortable">
-
-        </div>
-        <div id='area8' class="col-sm-3 connectedSortable">
-
-        </div>
-    </div>
-
-    <div class='statistic-row row'>
-        <div id='area9' class="col-sm-12 connectedSortable">
-
-        </div>
-@endif
-    </div>
-
-</div><!--END STATISTIC AREA-->
+    @endif
+</div>
 
 <script defer>
-
 if (window.location.href.includes('statistic_builder/builder')) {
-
-    //selectd td elements with id containing 'area'
-    const areas = document.querySelectorAll('td[id^="area"]');
-
-    areas.forEach(area => {
+    document.querySelectorAll('td[id^="area"]').forEach(area => {
         area.style.border = '1px solid black';
     });
-
 }
-
 </script>
